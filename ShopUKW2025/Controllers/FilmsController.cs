4d08ffe7966df1d92276351b1cc29a1a2d53fdba
apiszczukw <apiszczukw@gmail.com>
2025-04-14 16:27:24 +0200

    Dodawanie nowego filmu wraz z plakatem

--- ShopUKW2025/Controllers/FilmsController.cs
+++ ShopUKW2025/Controllers/FilmsController.cs
@@ -1,18 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.EntityFrameworkCore;
 using ShopUKW2025.DAL;
 using ShopUKW2025.Migrations;
+using ShopUKW2025.Models;
 
 namespace ShopUKW2025.Controllers
 {
     public class FilmsController : Controller
     {
         FilmsContext db;
+        IWebHostEnvironment webHost;
 
-		public FilmsController(FilmsContext db)
-		{
-			this.db = db;
-		}
+        public FilmsController(FilmsContext db, IWebHostEnvironment webHost)
+        {
+            this.db = db;
+            this.webHost = webHost;
+        }
+
+        [HttpGet]
+        public IActionResult AddFilm()
+        {
+            var model = new AddFilmViewModel();
+            model.AllCategories = db.Categories.ToList();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult AddFilm(AddFilmViewModel model)
+        {
+           
+
+            var picFolderPath = Path.Combine(webHost.WebRootPath, "obrazki");
+
+            var uniqueName = Guid.NewGuid() + "_" + model.Poster.FileName;
+
+            var filePath = Path.Combine(picFolderPath, uniqueName);
+
+            model.Poster.CopyTo(new FileStream(filePath, FileMode.Create));
+
+            model.NewFilm.Poster = uniqueName;
+
+            db.Films.Add(model.NewFilm);
+            db.SaveChanges();
+
+            return RedirectToAction("Details", new { filmId = model.NewFilm.FilmId });
+        }
+
 
         public IActionResult AllFilms()
         {
---
+++ ShopUKW2025/Models/AddFilmViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopUKW2025.Models
+{
+    public class AddFilmViewModel
+    {
+        public List<Category> AllCategories { get; set; }
+
+        public Film NewFilm { get; set; }
+
+        public IFormFile Poster { get; set; }
+    }
+}
